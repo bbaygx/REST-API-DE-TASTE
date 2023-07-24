f935889515bb17c8a266d296d6272155419e2c6d
Bayu Putra Efendi <91212372+bbaygx@users.noreply.github.com>
2023-07-24 22:49:09 +0700

    Update restaurantModel.js

--- src/models/restaurantModel.js
+++ src/models/restaurantModel.js
@@ -38,6 +38,10 @@ const restaurantSchema = new Schema({
     type: Boolean,
     required: true,
   },
+  terjual: {
+    type: Boolean,
+    required: true,
+  },
   createdAt: {
     type: Date,
     default: Date.now,
