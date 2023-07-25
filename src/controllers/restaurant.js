46ef9443ab56c7dae53b06b5af99d50d5760ca04
Bayu Putra Efendi <91212372+bbaygx@users.noreply.github.com>
2023-07-25 08:37:54 +0700

    Update restaurant.js

--- src/controllers/restaurant.js
+++ src/controllers/restaurant.js
@@ -8,7 +8,7 @@ export const getRestaurantsFood = async (req, res) => {
       region: { $regex: region || "", $options: "i" },
       status: { $regex: status || "", $options: "i" },
       foodType: { $regex: type || "", $options: "i" },
-      terlaris: req.query.terlaris || false,
+      terlaris: req.query.terlaris,
     });
     if (restaurants.length === 0)
       return res.status(404).json({
